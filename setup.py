3ef40710c3bdf006f62f2699ecedf7c0c8a26bd4
Clément Robert <cr52@protonmail.com>
2023-07-30 10:50:16 +0200

    BLD: enable ABI3 forward-compatible wheels for CPython

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 __author__ = "Konstantin Weddige"
+import sysconfig
 from setuptools import setup, Extension
+
+
+MINIMAL_PYTHON_VERSION = major, minor = (3, 6)
+
+# build with Py_LIMITED_API unless in freethreading build (which does not currently
+# support the limited API in py313t)
+USE_PY_LIMITED_API = not sysconfig.get_config_var("Py_GIL_DISABLED")
+define_macros = []
+
+if USE_PY_LIMITED_API:
+    define_macros.append(("Py_LIMITED_API", f"0x{major:02X}{minor:02X}0000"))
+    options = {"bdist_wheel": {"py_limited_api": f"cp{major}{minor}"}}
+else:
+    options = {}
 
 with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -21,8 +36,11 @@ setup(
             ["src/miniballmodule.cpp"],
             include_dirs=["src"],
             language="c++",
+            define_macros=define_macros,
+            py_limited_api=USE_PY_LIMITED_API,
         ),
     ],
+    python_requires=_get_python_requires(),
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python",
@@ -31,4 +49,5 @@ setup(
         "Topic :: Software Development :: Libraries",
         "Topic :: Utilities",
     ],
+    options=options,
 )
